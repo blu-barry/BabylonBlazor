18f02fd04234d5271b73e0700ac197cb2ac0665c
blu-barry <michael23barry@gmail.com>
2022-10-18 14:22:59 -0400

    Added an example of receiving input and output from js function in C#.

--- BabylonBlazorDemo/wwwroot/js/interopdemo.js
+++ BabylonBlazorDemo/wwwroot/js/interopdemo.js
@@ -1,3 +1,11 @@
 ﻿function createAlert() {
     alert("Hey this is an alert")
 }
+
+function createPrompt(question) {
+    return prompt(question);
+}
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
